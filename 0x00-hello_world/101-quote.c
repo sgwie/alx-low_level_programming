33615e73d6d9c25050074c5c4e05ed549df21872
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-09-08 13:59:37 +0000

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,7 @@
  */
 int main(void)
 {
-  char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-  write(2, quote, 59);
-  return (1);
+char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, quote, 59);
+return (1);
 }

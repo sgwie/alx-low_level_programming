0289e49f552b294c0050fa5e6d573fd2a724f0c0
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-09-09 10:09:55 +0000

    Prints lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *main - Prints small letter alphabet characters
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar (c);
+return (0);
+}

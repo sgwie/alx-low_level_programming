2a7aa7336506ae4288a30a1c8de00c912ba93371
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-09-09 09:39:32 +0000

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,11 +9,10 @@
  */
 int main(void)
 {
-int n;
+int a,n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-int a;
 a = n % 10;
 if (a > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, a);

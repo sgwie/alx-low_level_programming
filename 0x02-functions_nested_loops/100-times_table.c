3735a772ccc8b42bbeb5a601b205c5b46752c206
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-09-13 17:17:03 +0000

    Prints up n timestable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,6 +7,8 @@
 void print_times_table(int n)
 {
   int a, b, x;
+  if (n >=0 && n <= 15)
+    {
   for (a = 0 ; a <= n ; a++)
     {
       _putchar(48);
@@ -15,16 +17,26 @@ void print_times_table(int n)
 	  x = a * b;
 	  _putchar(44);
 	  _putchar(32);
-	  if (x < n)
+	  if (x <= 9)
 	    {
+	      _putchar(32);
 	      _putchar(32);
 	      _putchar(x + 48);
 	    }
+	  else if (x <=99)
+	    {
+	      _putchar(32);
+	      _putchar((x / 10) + 48);
+	      _putchar((x % 10) + 48);
+	    }
 	  else
 	    {
-	      _putchar((x / n) + 48);
-	      _putchar((x % n) + 48);
-	    }      }
+	      _putchar(((x / 100) % 10) + 48);
+	      _putchar(((x / 10) % 10) + 48);
+	      _putchar((x % 10) + 48);
+	    }
+	}
       _putchar('\n');
+    }
     }
 }

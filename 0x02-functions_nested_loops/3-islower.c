c00c74ebd19562aac8d453d477bbf80c0a6e0034
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-09-13 11:46:05 +0000

    Checks if lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_islower -Checks whether is lowercase
+ *
+ *Return: 1 if lowecase, 0 otherwise
+ */
+int _islower(int c)
+{
+if (c < 97 && c > 122)
+return (0);
+else
+return (1);
+}

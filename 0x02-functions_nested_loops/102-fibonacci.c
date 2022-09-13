0c808b6f80c705716b6ea7ac0e65e67d73c3424c
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-09-13 18:08:58 +0000

    First 50 Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Prints first 50 fibonacci terms
+ *
+ *Return: 0
+ */
+int main(void)
+{
+int i;
+long int t1 = 1, t2 = 2, nt;
+printf("%ld, %ld, ", t1, t2);
+for (i = 0; i <= 47; ++i)
+{
+nt = t1 + t2;
+printf(", %ld", nt);
+t1 = t2;
+t2 = nt;
+}
+printf("\n");
+return (0);
+}

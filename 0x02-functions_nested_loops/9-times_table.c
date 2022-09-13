327a6f015c779969a7be88809bffda49da07686e
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-09-13 16:27:31 +0000

    Timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,22 +9,22 @@ void times_table(void)
 int a, b, x;
 for (a = 0 ; a < 10 ; a++)
 {
-putchar(48);
+_putchar(48);
 for (b = 1; b < 10; b++)
 {
 x = a * b;
-putchar(44);
-putchar(32);
+_putchar(44);
+_putchar(32);
 if (x < 10)
 {
-putchar(32);
-putchar(x + 48);
+_putchar(32);
+_putchar(x + 48);
 }
 else
 {
-putchar((x / 10) + 48);
-putchar((x % 10) + 48);
+_putchar((x / 10) + 48);
+_putchar((x % 10) + 48);
 }      }
-putchar('\n');
+_putchar('\n');
 }
 }

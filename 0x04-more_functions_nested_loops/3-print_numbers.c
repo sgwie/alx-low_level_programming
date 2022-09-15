8cba38b6e43907e00cdb42cec4d3c7ded119ba40
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-09-15 12:50:43 +0000

    Prints 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_number - Prints numbers 0 to 9
+ *
+ *Return: Void
+ */
+
+void print_numbers(void)
+{
+int a;
+for (a = 48; a <= 57; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}

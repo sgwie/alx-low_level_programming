82211c20bdcc9eeb9daedd146169cbd259c3c9ea
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-09-15 08:27:12 +0000

    Largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -19,17 +19,15 @@ int largest;
 if (a > b && a > c)
 {
 largest = a;
-printf("%d\n is the largest number", a);
 }
 else if (b > a && b > c)
 {
 largest = b;
-printf("%d\n is the largest number", b);
 }
 else
 {
 largest = c;
-printf("%d\n is the largest number", c);
 }
 return (largest);
+printf("%d \n is the largest number" , largest);
 }

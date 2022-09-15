f463cd98a35c69051d6fc43cf9ed33ee8d6725db
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-09-15 15:43:42 +0000

    Print diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_diagonal - Prints diagonal line
+ *
+ *@n: Integer value
+ *
+ *Return: Void
+ */
+
+void print_diagonal(int n)
+{
+int a = 0, b;
+while (a < n && n > 0)
+{
+b = 0;
+while (b < a)
+{
+_putchar(' ');
+b++;
+}
+_putchar('\\');
+_putchar('\n');
+i++;
+}
+if (a == 0)
+_putchar('\n');
+}

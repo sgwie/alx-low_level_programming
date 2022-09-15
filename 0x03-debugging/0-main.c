48be74c9d481bb72b413c05e44ad5eb1dcc0b169
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-09-15 07:11:51 +0000

    Test integer

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * main - Tests function that prints if integer is positive or negative
+ * main - Tests function that prints if integer is positive
+ *        or negative
+ *
  * Return: 0
  */
 

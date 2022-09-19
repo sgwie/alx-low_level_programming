06aa1011befd510dfd91fb7e6cc45fef17a5a94f
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-09-19 11:34:06 +0000

    Swap two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *swap_int - Swaps two integers
+ *
+ *@a: Integer
+ *@b: Integer
+ *
+ *Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+int x, y;
+x = *a;
+y = *b;
+x = *b;
+y = *a;
+}

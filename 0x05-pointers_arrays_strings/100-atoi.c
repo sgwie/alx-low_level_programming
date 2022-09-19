fa0adccb76898b6739a1c89db9c92c9e540a96ca
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-09-19 18:12:39 +0000

    Convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,15 +13,13 @@ int _atoi(char *s)
 int sign = 1;
 unsigned int number = 0;
 
-do
-{
+do {
 if (*s == '-')
 sign *= -1;
 else if (*s >= '0' && *s <= '9')
 number = (number * 10) + (*s - '0');
 else if (number > 0)
 break;
-}
-while (*s++);
-return (number * sign);
+} while (*s++);
+return (number *sign);
 }

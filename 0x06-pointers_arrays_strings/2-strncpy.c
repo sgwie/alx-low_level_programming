79286b86b361ceb4b72b10a47f0b2023196f34b4
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-09-21 20:04:50 +0000

    Copies string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,18 +13,16 @@
 char *_strncpy(char *dest, char *src, int n)
 
 {
-  int i;
+int i;
 
-  for (i = 0; i < n && src[i] != '\0'; i++)
-    {
-      dest[i] = src[i];
-    }
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
 
-  for (; i < n; i++)
-    {
-      dest[i] = '\0';
-    }
-
-  return (dest);
-
+for (; i < n; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
 }

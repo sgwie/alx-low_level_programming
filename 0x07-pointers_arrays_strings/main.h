6516ab738c86d614c4d85eef823901401a119540
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-09-26 19:38:57 +0000

    Locate substring

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#ifndef main_h_
+0;10;1c0;10;1c0;10;1c#ifndef main_h_
 #define main_h_
 
 char *_memset(char *s, char b, unsigned int n);
@@ -6,5 +6,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 #endif

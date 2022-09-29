70007f816ac93b0ce3efb871ecbda3657304596b
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-09-29 22:30:23 +0000

    Square root of number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Computes square root
+ *
+ * @n:Integer value
+ *
+ * Return: Computed value
+ */
+
+int _sqrt_recursion(int n)
+{
+return (square(n, 1));
+}

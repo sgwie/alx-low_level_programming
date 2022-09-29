05293eff3aebb7990fa43f83e8bd332cb4263b65
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-09-29 22:57:43 +0000

    Square root of number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,3 +12,21 @@ int _sqrt_recursion(int n)
 {
 return (square(n, 1));
 }
+/**
+ * square - Find square root
+ *
+ * @n: Integer input
+ * @b: Integer
+ *
+ * Return: Value integer
+ */
+
+int square(int n, int b)
+{
+if (b * b == n)
+return (b);
+else if (b * b < n)
+return  (square(n, b + 1));
+else
+return (-1);
+}

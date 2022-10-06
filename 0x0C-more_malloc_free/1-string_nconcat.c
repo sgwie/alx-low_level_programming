293007c2432d50e18aa3ad8e31f58a290ae61134
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-10-06 17:05:39 +0000

    Concetenates two strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_strlen - Counts string length
+ *
+ *@s: String
+ *
+ * Return: Length of string
+ */
+
+int _strlen(char *s)
+{
+  unsigned int c = 0;
+  while (s[c] != '\0')
+    c++;
+  return (c);
+}
+/**
+ *str_concat- Concatenates two strings
+ *@s1: String
+ *@s2: String
+ *Return: Concatenated string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+  char *pointer;
+  unsigned int i, j, size;
+  if (s1 == NULL)
+    s1 = "";
+  if (s2 == NULL)
+    s2 = "";
+  size = (_strlen(s1) + _strlen(s2) + 1);
+  pointer = (char *) malloc(size *sizeof(char));
+  if (pointer == 0)
+    {
+      return (NULL);
+    }
+  for (i = 0; *(s1 + i) != '\0'; i++)
+    *(pointer + i) = *(s1 + i);
+  for (j = 0; j < n && *(s2 + j) != '\0'; j++)
+    {
+      *(pointer + i) = *(s2 + j);
+      i++;
+    }
+  return (pointer);
+}

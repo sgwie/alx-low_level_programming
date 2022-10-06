25fd5aa4a57f980905aefa90d99c281f8e098115
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-10-06 15:28:28 +0000

    Concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -35,7 +35,7 @@ s1 = "";
 if (s2 = NULL)
 s2 = "";
 size = (_strlen(s1) + _strlen(s2) + 1);
-pointer = malloc(size * sizeof(char));
+pointer = (char*) malloc(size * sizeof(char));
 if (pointer == 0)
 return (NULL);
 for (i = 0; s1[i] != '\0'; i++)

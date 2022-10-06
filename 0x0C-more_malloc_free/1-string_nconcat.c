090d31801d61b2bd1657c3d8e1914a3dfc319397
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-10-06 18:54:45 +0000

    Concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,39 +11,44 @@
 
 int _strlen(char *s)
 {
-  unsigned int c = 0;
-  while (s[c] != '\0')
-    c++;
-  return (c);
+unsigned int c = 0;
+while (s[c] != '\0')
+c++;
+return (c);
 }
+
 /**
- *str_concat- Concatenates two strings
+ *string_nconcat- Concatenates two strings
+ *
  *@s1: String
  *@s2: String
+ *
  *Return: Concatenated string
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  char *pointer;
-  unsigned int i, j, size;
-  if (s1 == NULL)
-    s1 = "";
-  if (s2 == NULL)
-    s2 = "";
-  if (n > _strlen(s2))
-    n = _strlen(s2);
-  size = _strlen(s1) + n;
-  pointer = malloc(size + 1);
-  if (pointer == NULL)
-    {
-      return (NULL);
-    }
-  for (i = 0; i < size; i++)
-    if (i < _strlen(s1))
-      *(pointer + i) = *(s1 + i);
-    else
-      pointer[i] = s2[i - _strlen(s1)];
-  pointer[i] = '\0';
-  return (pointer);
+char *pointer;
+unsigned int i, j, size;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+if (n > _strlen(s2))
+n = _strlen(s2);
+size = _strlen(s1) + n;
+pointer = malloc(size + 1);
+if (pointer == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+if (i < _strlen(s1))
+*(pointer + i) = *(s1 + i);
+else
+pointer[i] = s2[i - _strlen(s1)];
+}
+pointer[i] = '\0';
+return (pointer);
 }

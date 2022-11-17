4b995ffa7953a9f8bdf8f84e54122b645891635a
Ndabezinhle Ngubhe Dube <ndabezinhle.ngubhe@gmail.com>
2022-11-17 11:58:03 +0000

    Print lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,20 +10,18 @@
 
 size_t print_list(const list_t *h)
 {
-  size_t node_count = 1;
-
-  if (h == NULL)
-    return (0);
-
-  while (h->next != NULL)
-    {
-      if (h->str == NULL)
-	printf("[%d] %s\n", 0, "(nil)");
-      else
-	printf("[%d] %s\n", h->len, h->str);
-      h = h->next;
-      node_count += 1;
-    }
-  printf("[%d] %s\n", h->len, h->str);
-  return (node_count);
+size_t node_count = 1;
+if (h == NULL)
+return (0);
+while (h->next != NULL)
+{
+if (h->str == NULL)
+printf("[%d] %s\n", 0, "(nil)");
+else
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+node_count += 1;
+}
+printf("[%d] %s\n", h->len, h->str);
+return (node_count);
 }
